7faa0f09ae2329c4d830cf5a75b0bbfedd4047cd
topicular <40355580+metnias@users.noreply.github.com>
2023-05-13 20:14:31 +0900

    Implemented Rope physics to map paper and made it a Prefab

--- Assets/Scripts/HandMap/HandMapController.cs
+++ Assets/Scripts/HandMap/HandMapController.cs
@@ -9,24 +9,14 @@ public class HandMapController : MonoBehaviour
     [SerializeField]
     private Transform handleRight = null;
 
-    [Header("Paper - InitValue")]
-    [SerializeField, Range(0.01f, 1f)]
-    private float paperHeight = 0.3f;
-
-    [SerializeField, Range(2, 50)]
-    private int paperSegsHorz = 5;
-    [SerializeField, Range(2, 50)]
-    private int paperSegsVert = 5;
-
     [SerializeField]
-    private Material paperMaterial = null;
+    private MapPaperMeshHandler.PaperInfo paperInitStats;
 
     private MapPaperMeshHandler paperHandler;
 
     private void Start()
     {
-        paperHandler = new MapPaperMeshHandler(this, handleLeft, handleRight, paperHeight, paperSegsHorz, paperSegsVert);
-        paperHandler.Renderer().material = paperMaterial;
+        paperHandler = new MapPaperMeshHandler(this, handleLeft, handleRight, paperInitStats);
     }
 
     private void Update()
--- Assets/Scripts/HandMap/MapPaperMeshHandler.cs
+++ Assets/Scripts/HandMap/MapPaperMeshHandler.cs
@@ -11,29 +11,51 @@ public class MapPaperMeshHandler
     private readonly int SEG_HORZ = 5;
     private readonly int SEG_VERT = 5;
 
-    public MapPaperMeshHandler(HandMapController controller, Transform handleLeft, Transform handleRight,
-        float paperHeight, int segHorz, int segVert)
+    [Serializable]
+    public struct PaperInfo
     {
-        SEG_HORZ = segHorz; SEG_VERT = segVert;
-        halfHeight = paperHeight * 0.5f;
+        [Range(2, 50)]
+        public int segHorz;
+        [Range(2, 50)]
+        public int segVert;
+        [Range(0f, 2f)]
+        public float height;
+        [Range(0.5f, 1.5f)]
+        public float loseness;
+        [Range(0f, 1f)]
+        public float tension;
+        [Range(0f, 1f)]
+        public float gravity;
+        [Range(0f, 10f)]
+        public float velClamp;
+        public Material mapMaterial;
+    }
+
+    public MapPaperMeshHandler(HandMapController controller, Transform handleLeft, Transform handleRight, PaperInfo info)
+    {
+        SEG_HORZ = info.segHorz; SEG_VERT = info.segVert;
+        halfHeight = info.height * 0.5f;
+        Rope.tension = info.tension; Rope.loseness = info.loseness; Rope.gravity = info.gravity / SEG_HORZ; Rope.velClamp = info.velClamp;
         this.controller = controller;
         this.handleLeft = handleLeft;
         this.handleRight = handleRight;
 
+        // 종이가 들어갈 GameObject 생성
         paperObject = new GameObject("MapPaper");
         paperObject.transform.SetParent(this.controller.transform);
         paperObject.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
 
-        // create two rope: top and bottom
+        // 위와 아래 로프 생성
         ropeTop = new Rope(this.handleLeft.localPosition + this.handleLeft.up * halfHeight, this.handleRight.localPosition + this.handleRight.up * halfHeight, SEG_HORZ);
         ropeBtm = new Rope(this.handleLeft.localPosition - this.handleLeft.up * halfHeight, this.handleRight.localPosition - this.handleRight.up * halfHeight, SEG_HORZ);
 
-        // initialize mesh
+        // 메쉬 초기화
         points = new Vector3[SEG_HORZ, SEG_VERT];
         mesh = new Mesh() { name = "MapPaperMesh" };
         var meshFilter = paperObject.AddComponent<MeshFilter>();
         meshFilter.mesh = mesh;
         renderer = paperObject.AddComponent<MeshRenderer>();
+        renderer.material = info.mapMaterial;
         InitializeMesh();
     }
 
@@ -49,8 +71,6 @@ public class MapPaperMeshHandler
     private readonly Mesh mesh;
     private readonly MeshRenderer renderer;
     private readonly Vector3[,] points;
-
-    public MeshRenderer Renderer() => renderer;
 
     private void InitializeMesh()
     {
@@ -72,9 +92,9 @@ public class MapPaperMeshHandler
     /// </summary>
     public void Update()
     {
-        Debug.DrawLine(handleLeft.position, handleRight.position);
+        // Debug.DrawLine(handleLeft.position, handleRight.position);
 
-        // Lerp b/w two ropes to get vertices
+        // 가운데 구간 버텍스의 위치를 선형 보간으로 구하기
         #region GetVertices
         var dists = new float[SEG_HORZ, SEG_VERT];
         var vertsTop = ropeTop.GetSegmentsPos();
@@ -92,7 +112,7 @@ public class MapPaperMeshHandler
         }
         #endregion GetVertices
 
-        // update vertices position
+        // 메쉬의 버텍스와 UV를 갱신
         List<Vector3> verts = new();
         List<Vector2> uvs = new();
         for (int u = 0; u < SEG_HORZ - 1; ++u)
@@ -120,12 +140,15 @@ public class MapPaperMeshHandler
     /// </summary>
     public void FixedUpdate()
     {
-        // update two rope: top and bottom
         ropeTop.Simulate(handleLeft.localPosition + handleLeft.up * halfHeight, handleRight.localPosition + handleRight.up * halfHeight);
         ropeBtm.Simulate(handleLeft.localPosition - handleLeft.up * halfHeight, handleRight.localPosition - handleRight.up * halfHeight);
     }
 
 
+    /// <summary>
+    /// 로프 시뮬레이션
+    /// (참조: https://youtu.be/FcnvwtyxLds)
+    /// </summary>
     private class Rope
     {
         public Rope(Vector3 posLeft, Vector3 posRight, int segments)
@@ -138,10 +161,17 @@ public class MapPaperMeshHandler
 
         private readonly Segment[] segments;
         private float segLength;
+        public static float loseness = 1f;
+        public static float tension = 1f;
+        public static float gravity = 1f;
+        public static float velClamp = 1f;
 
+        /// <summary>
+        /// 로프의 위치를 선형 보간으로 리셋
+        /// </summary>
         public void Reset(Vector3 posLeft, Vector3 posRight)
         {
-            segLength = Vector3.Distance(posLeft, posRight) / (segments.Length - 1);
+            segLength = loseness * Vector3.Distance(posLeft, posRight) / (segments.Length - 1);
             for (int i = 0; i < segments.Length; ++i)
             {
                 segments[i].pos = Vector3.Lerp(posLeft, posRight, (float)i / (segments.Length - 1));
@@ -149,12 +179,53 @@ public class MapPaperMeshHandler
             }
         }
 
+        /// <summary>
+        /// 로프 물리 기반 시뮬레이션
+        /// </summary>
         public void Simulate(Vector3 posLeft, Vector3 posRight)
         {
-            // TODO: simulate rope physics
-            Reset(posLeft, posRight); // temp
+            //Reset(posLeft, posRight); return;
+            segLength = loseness * Vector3.Distance(posLeft, posRight) / (segments.Length - 1);
+            var grav = gravity * Time.fixedDeltaTime * Physics.gravity;
+            for (int i = 0; i < segments.Length; ++i)
+            {
+                var vel = segments[i].pos - segments[i].lastPos;
+                segments[i].lastPos = segments[i].pos;
+                vel = Vector3.ClampMagnitude(vel + grav, velClamp * segLength);
+                segments[i].pos += vel;
+            }
+
+            // constraints
+            segments[0].pos = posLeft;
+            segments[^1].pos = posRight;
+            for (int i = 0; i < segments.Length - 1; ++i)
+            {
+                float dist = (segments[i].pos - segments[i + 1].pos).magnitude;
+                float error = Mathf.Abs(dist - segLength);
+
+                var tensionDir = Vector3.zero;
+                if (dist > segLength)
+                    tensionDir = (segments[i].pos - segments[i + 1].pos).normalized;
+                else if(dist < segLength)
+                    tensionDir = (segments[i + 1].pos - segments[i].pos).normalized;
+
+                var tensionVel = error * tension * tensionDir;
+                if (i == 0)
+                    segments[i + 1].pos += tensionVel;
+                else if (i == segments.Length - 1)
+                    segments[i].pos -= tensionVel;
+                else
+                {
+                    tensionVel *= 0.8f;
+                    segments[i].pos -= tensionVel;
+                    segments[i + 1].pos += tensionVel;
+                }
+            }
         }
 
+        /// <summary>
+        /// 로프 구간의 각 위치를 배열로 반환
+        /// </summary>
         public Vector3[] GetSegmentsPos()
             => Array.ConvertAll(segments, x => x.pos);
 
@@ -163,7 +234,7 @@ public class MapPaperMeshHandler
             public Segment(Vector3 pos)
             {
                 this.pos = pos;
-                this.lastPos = pos;
+                lastPos = pos;
             }
 
             public Vector3 pos, lastPos;
