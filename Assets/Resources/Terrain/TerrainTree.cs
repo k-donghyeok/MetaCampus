64f439d8e34717dc2f35d7d28f77911383039628
kdw98tg <rkdehdgur432@naver.com>
2023-05-16 19:34:51 +0900

    terrain 제작중
    건물배치 완료 도로 2개 배치완료

---
+++ Assets/Resources/Terrain/TerrainTree.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainTree : MonoBehaviour
+{
+    Collider myCollider;
+
+    private void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        Debug.Log($"Test: {myCollider}");
+    }
+
+    void Update()
+    {
+        if (!Input.GetMouseButton(0)) return;
+        if(! Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var info)) return;
+        if (info.collider != myCollider) return;
+        gameObject.SetActive(false);
+    }
+}
