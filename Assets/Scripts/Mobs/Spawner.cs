37578beaf1aed0d31056d32d54ef639429938951
Metnias <40355580+metnias@users.noreply.github.com>
2023-05-17 13:39:31 +0900

    More setup for MobSpawner

--- Assets/Scripts/Mobs/Spawner.cs
+++ Assets/Scripts/Mobs/Spawner.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject mobPrefab = null;
+
+    [SerializeField, Range(0, 100)]
+    private int amount = 1;
+
+    private Mob[] mobPool;
+
+    private bool init = false;
+
+    private void Awake()
     {
-        
+        init = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Activate()
     {
-        
+        if (!init)
+        {
+            mobPool = new Mob[amount];
+            for (int i = 0; i < amount; ++i)
+            {
+                var go = Instantiate(mobPrefab);
+                go.transform.SetParent(transform, true);
+                go.transform.SetLocalPositionAndRotation(GetRandomSpawnPos(), Quaternion.Euler(0f, Random.Range(0f, 360f), 0f));
+
+            }
+
+            init = true;
+        }
+    }
+
+    private Vector3 GetRandomSpawnPos()
+    {
+        var rad = GetComponent<SphereCollider>().radius;
+        Vector3 pos = Vector3.zero;
+        while (true)
+        {
+            //pos = Vector3.up * rad + 
+        }
+
+        return pos;
+    }
+
+    public void Deactivate()
+    {
+
     }
 }
