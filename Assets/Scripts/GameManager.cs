b299ac563b15d87c55dc14d18533fb876d751f69
castleming <psmlotte@gmail.com>
2023-05-24 14:49:40 +0900

    respawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,9 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public static GameManager Instance() => instance;
 
+    private SaveManager saveManager;
+    private SpawnManager spawnManager;
+
     private void Awake()
     {
         if (instance == null)
@@ -23,40 +26,14 @@ public class GameManager : MonoBehaviour
         else if (instance != this) Destroy(gameObject);
     }
 
-    private void Start()
-    {
-        CheckSPAWN("asd");
-        //체크진행도
-        //체크지도내용
-    }
-
-    //스폰포인트가있는지확인하는함수
-    private void CheckSPAWN(string _spawnid)
-    {
-        int a = Save.LoadValue(_spawnid, -1);
-
-        if( a ==-1) //세이브가 없을떄
-        {
-            return;
-        }
-
-        //세이브가 있을떄
-        if(a !=-1)
-        {
-            //플레이어 포지션을 id의 포지션으로 변경
-        }
-
-    }
-        
-
     //진행도가있는지 확인하는 함수
 
-        //지도내용 변경된게있는지 확인하는함수
+    //지도내용 변경된게있는지 확인하는함수
 
-        /// <summary>
-        /// 점수 관리
-        /// </summary>
-    public YeilManager Yeil { get; private set; } = null;
+    /// <summary>
+    /// 점수 관리
+    /// </summary>
+    public YeilManager Yeil { get; private set; } = null;   
 
     /// <summary>
     /// 저장 관리
@@ -73,8 +50,25 @@ public class GameManager : MonoBehaviour
         Yeil = new YeilManager();
         Spawn = new SpawnManager();
         Scene = new MySceneManager();
-       
+
         Save.Initialize();
+        Spawn.Initialize();
     }
 
+    private void Start()
+    {
+        // Place the player at a random spawn point when the game starts
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        spawnManager.PlacePlayerAtRandomSpawnPoint(player);
+    }
+
+    public SaveManager GetSaveManager()
+    {
+        return saveManager;
+    }
+
+    public SpawnManager GetSpawnManager()
+    {
+        return spawnManager;
+    }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,14 +1,53 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager 
+public class SpawnManager
 {
-    //저장된 스폰 포인트 불러오기
+    private const string SPAWN_POINT_KEY = "SpawnPoint";
 
-    //스폰포인트 저장하기
+    private List<Vector3> spawnPoints = new List<Vector3>();
 
-    //플레이어 이동시키기
+    public SpawnManager()
+    {
+    }
 
-   
+    public void Initialize()
+    {
+        LoadSpawnPoints();
+    }
+
+    /// <summary>
+    /// 스폰 포인트 저장
+    /// </summary>
+    public void SaveSpawnPoints(List<Vector3> points)
+    {
+        spawnPoints = points;
+        SaveManager.Instance().SaveValue(SPAWN_POINT_KEY, points);
+    }
+
+    /// <summary>
+    /// 스폰 포인트 불러오기
+    /// </summary>
+    public List<Vector3> LoadSpawnPoints()
+    {
+        spawnPoints = SaveManager.Instance().LoadValue<List<Vector3>>(SPAWN_POINT_KEY, new List<Vector3>());
+        return spawnPoints;
+    }
+
+    /// <summary>
+    /// Place the player at a random spawn point.
+    /// </summary>
+    public void PlacePlayerAtRandomSpawnPoint(GameObject player)
+    {
+        if (spawnPoints.Count > 0)
+        {
+            int randomIndex = Random.Range(0, spawnPoints.Count);
+            Vector3 spawnPoint = spawnPoints[randomIndex];
+            player.transform.position = spawnPoint;
+        }
+        else
+        {
+            Debug.LogWarning("No spawn points available.");
+        }
+    }
 }
