a97dedf28cf3449086f8c35e8e90f34f07dd3e60
kdw98tg <rkdehdgur432@naver.com>
2023-05-24 12:25:22 +0900

    스폰매니저,씬매니저 작업시작

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,9 +23,39 @@ public class GameManager : MonoBehaviour
         else if (instance != this) Destroy(gameObject);
     }
 
-    /// <summary>
-    /// 점수 관리
-    /// </summary>
+    private void Start()
+    {
+        CheckSPAWN("asd");
+        //체크진행도
+        //체크지도내용
+    }
+
+    //스폰포인트가있는지확인하는함수
+    private void CheckSPAWN(string _spawnid)
+    {
+        int a = Save.LoadValue(_spawnid, -1);
+
+        if( a ==-1) //세이브가 없을떄
+        {
+            return;
+        }
+
+        //세이브가 있을떄
+        if(a !=-1)
+        {
+            //플레이어 포지션을 id의 포지션으로 변경
+        }
+
+    }
+        
+
+    //진행도가있는지 확인하는 함수
+
+        //지도내용 변경된게있는지 확인하는함수
+
+        /// <summary>
+        /// 점수 관리
+        /// </summary>
     public YeilManager Yeil { get; private set; } = null;
 
     /// <summary>
@@ -33,10 +63,16 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public SaveManager Save { get; private set; } = null;
 
+    public SpawnManager Spawn { get; private set; } = null;
+
+    public MySceneManager Scene { get; private set; } = null;
+
     private void Initiate()
     {
         Save = new SaveManager();
         Yeil = new YeilManager();
+        Spawn = new SpawnManager();
+        Scene = new MySceneManager();
        
         Save.Initialize();
     }
