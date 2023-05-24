142fc111671d9bd648aa995d27fe9151a3749228
Metnias <40355580+metnias@users.noreply.github.com>
2023-05-24 12:38:20 +0900

    Implemented Pen take out

--- Assets/Scripts/Player/InkPen/InkPenManager.cs
+++ Assets/Scripts/Player/InkPen/InkPenManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class InkPenManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    internal PlayerManager player = null;
+
+    internal HandMapManager Map => player.Map();
+
+    [SerializeField]
+    internal Transform point = null;
+
+    public void SetHeld(bool held)
     {
-        
+        if (Held == held) return;
+        Held = held;
+        hideTimer = 1f;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 이것이 플레이어 손에 들려있는지 여부
+    /// </summary>
+    public bool Held { get; private set; }
+
+    private float hideTimer = 1f;
+
+    private void Update()
     {
-        
+        if (!Held)
+        {
+            hideTimer -= Time.deltaTime;
+            if (hideTimer <= 0f) gameObject.SetActive(false);
+            return;
+        }
+
+
     }
 }
--- Assets/Scripts/Player/InkPen/InkPenTakeOut.cs
+++ Assets/Scripts/Player/InkPen/InkPenTakeOut.cs
@@ -1,18 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
+/// <summary>
+/// ÆæÀ» ²¨³»´Â Çàµ¿ Ã³¸®
+/// </summary>
+[RequireComponent(typeof(GrabActionHandler))]
 public class InkPenTakeOut : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private InkPenManager pen = null;
+    private GrabActionHandler grabActionHandler = null;
+
+    private XRDirectInteractor[] DirectInteractors => grabActionHandler.directInteractors;
+
+    private void Start()
     {
-        
+        if (!pen)
+            throw new ArgumentNullException(nameof(pen));
+        grabActionHandler = GetComponent<GrabActionHandler>();
+        penHand = null;
+
+        grabActionHandler.OnGrabbed += (left, device) =>
+        {
+            if (penHand.HasValue || grabActionHandler.GrabOccupied(left)) return;
+            if (!device.TryGetFeatureValue(CommonUsages.devicePosition, out var pos)) return;
+            if (pos.y < 1.0f) return; // too low
+            if (pos.z > 0.1f) return; // too front
+
+            TakeOutPen(left);
+        };
+        grabActionHandler.OnGrabReleased += (left, device) =>
+        {
+            if (penHand != left) return;
+            ReleasePen();
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool? penHand = null;
+
+    private void TakeOutPen(bool left)
     {
-        
+        penHand = left;
+        pen.gameObject.SetActive(true);
+        pen.SetHeld(true);
+        pen.transform.SetParent(DirectInteractors[left ? 0 : 1].attachTransform);
+        grabActionHandler.RequestHandAnimation(left, HandAnimator.SpecialAnimation.GripPhone);
+        if (left)
+            pen.transform.SetLocalPositionAndRotation(Vector3.right * 0f, Quaternion.Euler(0f, 0f, 0f));
+        else
+            pen.transform.SetLocalPositionAndRotation(Vector3.left * 0f, Quaternion.Euler(0f, 0f, 0f));
+        pen.GetComponent<Rigidbody>().useGravity = false;
+        pen.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+
+    private void ReleasePen()
+    {
+        grabActionHandler.RequestHandAnimation(penHand.Value, HandAnimator.SpecialAnimation.None);
+        penHand = null;
+        pen.transform.SetParent(null);
+        pen.GetComponent<Rigidbody>().useGravity = true;
+        pen.SetHeld(false);
     }
 }
