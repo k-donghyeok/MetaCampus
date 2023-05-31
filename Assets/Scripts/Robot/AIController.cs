36addc312b6ef78d52c3ec7af44db96a5cb082ca
castleming <psmlotte@gmail.com>
2023-05-31 15:38:42 +0900

    AI 이동 수정중

--- Assets/Scripts/Robot/AIController.cs
+++ Assets/Scripts/Robot/AIController.cs
@@ -1,131 +1,91 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
-    public Transform[] waypoints;
-    public float moveSpeed = 3f;
-    public float waypointWaitTime = 2f;
-    public float detectionRadius = 5f;
-    public float detectionAngle = 60f;
-    public LayerMask playerLayer;
-    public LayerMask obstacleLayer;
+    public Transform startPoint; // 시작 지점
+    public Transform endPoint; // 도착 지점
 
-    private int currentWaypointIndex = 0;
-    private bool isWaiting = false;
-    private float waitTimer = 0f;
-    private bool playerDetected = false;
-    private float detectionCooldownTimer = 0f;
-    private float detectionCooldownDuration = 5f;
+    public float visionRadius = 10f; // 시야 반경
+    public float visionAngle = 60f; // 시야 각도
+    public LayerMask playerLayer; // Player 레이어
+    public float pauseDuration = 5f; // 멈추는 시간
+
+    private NavMeshAgent agent;
+    private bool isMovingToStartPoint = true;
+    private bool isPaused = false;
+    private float pauseTimer = 0f;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(startPoint.position);
+    }
 
     private void Update()
     {
-        // 플레이어를 감지한 상태인 경우 감지 쿨다운 타이머를 업데이트하고, 쿨다운이 끝나면 감지 상태를 해제
-        if (playerDetected)
+        if (!isPaused)
         {
-            detectionCooldownTimer += Time.deltaTime;
-            if (detectionCooldownTimer >= detectionCooldownDuration)
+            if (agent.remainingDistance <= agent.stoppingDistance)
             {
-                playerDetected = false;
-                detectionCooldownTimer = 0f;
-            }
-        }
-        // AI가 대기 중이 아닌 경우 동작
-        if (!isWaiting)
-        {
-            // 플레이어를 아직 감지하지 않은 경우
-            if (!playerDetected)
-            {
-                // AI 주변의 플레이어 레이어를 가진 콜라이더를 검사
-                Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRadius, playerLayer);
-                if (colliders.Length > 0)
+                if (isMovingToStartPoint)
                 {
-                    foreach (Collider collider in colliders)
-                    {
-                        Vector3 directionToPlayer = collider.transform.position - transform.position;
-                        directionToPlayer.y = 0f;
-                        float angle = Vector3.Angle(transform.forward, directionToPlayer);
-                        // 각도가 시야 각도의 절반 이하이고, AI와 플레이어 사이에 장애물이 없는 경우
-                        if (angle <= detectionAngle * 0.5f)
-                        {
-                            if (!HasObstacleInBetween(transform.position, collider.transform.position))
-                            {
-                                Debug.Log("플레이어 발견!");
-                                playerDetected = true;
-                                break;
-                            }
-                        }
-                    }
+                    agent.SetDestination(endPoint.position);
+                    isMovingToStartPoint = false;
+                }
+                else
+                {
+                    agent.SetDestination(startPoint.position);
+                    isMovingToStartPoint = true;
                 }
             }
-            // 플레이어를 감지하지 않은 경우 현재 웨이포인트를 향해 회전
-            if (!playerDetected && currentWaypointIndex < waypoints.Length)
-            {
-                Vector3 currentWaypointPosition = waypoints[currentWaypointIndex].position;
-                Vector3 directionToWaypoint = currentWaypointPosition - transform.position;
-                directionToWaypoint.y = 0f;
-                if (directionToWaypoint != Vector3.zero)
-                {
-                    Quaternion targetRotation = Quaternion.LookRotation(directionToWaypoint);
-                    transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360f * Time.deltaTime);
-                }
-            }
-            // 플레이어를 감지하지 않은 경우 현재 웨이포인트로 이동
-            if (!playerDetected)
-            {
-                Vector3 targetPosition = waypoints[currentWaypointIndex].position;
-                transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
 
-                // 현재 웨이포인트에 도착한 경우 대기 상태로 전환
-                if (transform.position == targetPosition)
+            // Player 감지
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, visionRadius, playerLayer);
+            foreach (Collider collider in hitColliders)
+            {
+                // Player의 방향 벡터 계산
+                Vector3 directionToPlayer = collider.transform.position - transform.position;
+                directionToPlayer.y = 0f; // y축 방향 제거
+
+                // Player가 시야 범위 내에 있는지 확인
+                if (Vector3.Angle(transform.forward, directionToPlayer) <= visionAngle / 2f)
                 {
-                    isWaiting = true;
-                    waitTimer = 0f;
-                    currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+                    // Player 감지 시 동작
+                    isPaused = true;
+                    pauseTimer = pauseDuration;
+                    agent.isStopped = true;
+                    Debug.Log("Player 감지됨!");
+                    break;
                 }
             }
         }
         else
         {
-            // 대기 중인 경우 대기 시간을 측정하고, 대기 시간이 끝나면 대기 상태를 해제하고 플레이어 감지 상태를 해제
-            waitTimer += Time.deltaTime;
-            if (waitTimer >= waypointWaitTime)
+            pauseTimer -= Time.deltaTime;
+            if (pauseTimer <= 0f)
             {
-                isWaiting = false;
-                playerDetected = false;
+                // 멈춤 시간 종료 후 다시 이동
+                isPaused = false;
+                agent.isStopped = false;
+                Debug.Log("이동 재개!");
             }
         }
     }
-    // 두 지점 사이에 장애물이 있는지 확인
-    private bool HasObstacleInBetween(Vector3 startPoint, Vector3 targetPoint)
-    {
-        Vector3 direction = targetPoint - startPoint;
-        Ray ray = new Ray(startPoint, direction);
-        float distance = Vector3.Distance(startPoint, targetPoint);
 
-        RaycastHit[] hits = Physics.RaycastAll(ray, distance, obstacleLayer);
-        foreach (RaycastHit hit in hits)
-        {
-            if (hit.collider != null && !hit.collider.isTrigger)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-    // 시각적으로 디버그하기 위해 기즈모를 그림
     private void OnDrawGizmosSelected()
     {
+        // 시야 범위를 에디터 상에서 시각적으로 표시
         Gizmos.color = Color.red;
-        // AI의 시야 범위를 기즈모로 표시
-        Vector3 detectionStart = transform.position + transform.forward * detectionRadius;
-        Vector3 detectionEndLeft = transform.position + Quaternion.Euler(0f, -detectionAngle * 0.5f, 0f) * transform.forward * detectionRadius;
-        Vector3 detectionEndRight = transform.position + Quaternion.Euler(0f, detectionAngle * 0.5f, 0f) * transform.forward * detectionRadius;
+        Vector3 direction = transform.forward;
+        Quaternion leftRayRotation = Quaternion.AngleAxis(-visionAngle / 2f, Vector3.up);
+        Quaternion rightRayRotation = Quaternion.AngleAxis(visionAngle / 2f, Vector3.up);
+        Vector3 leftRayDirection = leftRayRotation * direction;
+        Vector3 rightRayDirection = rightRayRotation * direction;
 
-        // AI의 시야 범위를 선으로 그림
-        Gizmos.DrawLine(transform.position, detectionStart);
-        Gizmos.DrawLine(transform.position, detectionEndLeft);
-        Gizmos.DrawLine(transform.position, detectionEndRight);
-        Gizmos.DrawLine(detectionEndLeft, detectionEndRight);
+        Gizmos.DrawRay(transform.position, leftRayDirection * visionRadius);
+        Gizmos.DrawRay(transform.position, rightRayDirection * visionRadius);
+        Gizmos.DrawRay(transform.position, direction * visionRadius);
+        
     }
 }
