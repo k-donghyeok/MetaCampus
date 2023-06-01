4ea5eccdb687e60975f571cb4ac017b19c9363d2
Metnias <40355580+metnias@users.noreply.github.com>
2023-06-01 16:53:15 +0900

    Added Map Wash mechanism

--- Assets/Scripts/Player/HandMap/CleanerController.cs
+++ Assets/Scripts/Player/HandMap/CleanerController.cs
@@ -1,19 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class CleanerController : MonoBehaviour
 {
     [SerializeField]
     private GameObject faucetPS = null;
 
-    private void OnTriggerEnter(Collider other)
+    private Collider faucetZone = null;
+
+    private PlayerManager player = null;
+
+    private void Start()
     {
-        
+        faucetZone = GetComponent<BoxCollider>();
     }
 
-    private void OnTriggerExit(Collider other)
+    private void Update()
     {
-        
+        if (!player) return;
+
+        if (player.Map().isActiveAndEnabled)
+            player.Map().WashUpdate(faucetZone);
+
+        if (Vector3.Distance(player.xrOrigin.position, transform.position) > 2f) Deactivate();
+    }
+
+    private void Activate()
+    {
+        faucetPS.SetActive(true);
+    }
+
+    private void Deactivate()
+    {
+        faucetPS.SetActive(false);
+        player = null;
+    }
+
+    public void OnHovered(XRBaseInteractable self)
+    {
+        var interactor = self.GetOldestInteractorHovering();
+        if (interactor == null) return;
+        var go = interactor.transform.root;
+        player = go.GetComponent<PlayerManager>();
+        if (player) Activate();
     }
 }
