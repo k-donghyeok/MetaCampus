5ac917ee7826db7e2da27da33e4a990ec64de88c
Metnias <40355580+metnias@users.noreply.github.com>
2023-06-01 17:28:26 +0900

    Tweaked ShortcutDoor trigger; Added FreeDoor prefab

---
+++ Assets/Scripts/Lock/Doors/FreeDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FreeDoor : DoorLock
+{
+    public void Toggle(XRBaseInteractable self)
+    {
+        var interactor = self.GetOldestInteractorHovering();
+        if (interactor == null) return;
+        var go = interactor.transform.root;
+        var pm = go.GetComponent<PlayerManager>();
+        if (pm == null) return;
+
+        /*
+        var dir = pm.xrOrigin.position - transform.position;
+        var dot = Vector3.Dot(dir, Clockwise ? transform.forward : -transform.forward);
+        if (dot < 0f) return; // Not Front
+
+        IsUnlocked = true;
+        PlayOpenAnimation();
+        self.enabled = false; */
+    }
+}
