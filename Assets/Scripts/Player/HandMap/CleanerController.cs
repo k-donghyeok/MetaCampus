1af87aed4c94ffcf94a3008511e534455a9da9c2
Metnias <40355580+metnias@users.noreply.github.com>
2023-06-01 15:29:42 +0900

    Added MapCleaner Prefab

---
+++ Assets/Scripts/Player/HandMap/CleanerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject faucetPS = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
