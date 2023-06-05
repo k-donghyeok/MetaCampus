bfe0fe34d55f7ab0d7ab90f9d3e6b04d35534b2a
Metnias <40355580+metnias@users.noreply.github.com>
2023-06-05 12:32:20 +0900

    Moved Skybox texture and Added LightManager

---
+++ Assets/Scripts/StageManager/LightManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    [Header("Skybox")]
+    [SerializeField]
+    private Skybox skyboxDay;
+    [SerializeField]
+    private Skybox skyboxNight;
+
+    [Header("Directional Lights")]
+    [SerializeField]
+    private GameObject dirLightDay;
+    [SerializeField]
+    private GameObject dirLightNight;
+
+    public void Initialize(bool day)
+    {
+        if (day)
+        {
+            dirLightDay.SetActive(true);
+            dirLightNight.SetActive(false);
+        }
+        else
+        {
+            dirLightDay.SetActive(false);
+            dirLightNight.SetActive(true);
+        }
+    }
+}
