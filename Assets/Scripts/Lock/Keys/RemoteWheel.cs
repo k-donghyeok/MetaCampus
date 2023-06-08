8a13333e6691918c836d961262413e81b6fcfd49
Metnias <40355580+metnias@users.noreply.github.com>
2023-06-08 10:48:06 +0900

    Applied RemoteLever new model

--- Assets/Scripts/Lock/Keys/RemoteWheel.cs
+++ Assets/Scripts/Lock/Keys/RemoteWheel.cs
@@ -3,7 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
 
 public class RemoteWheel : RemoteKeyBase
 {
+    [SerializeField, Range(1, 20)]
+    private int spinAmount = 5;
+
+    [SerializeField]
+    private TMP_Text txtLeft;
+
+    private int spinLeft;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        spinLeft = spinAmount;
+        txtLeft.text = spinLeft.ToString();
+    }
+
 }
