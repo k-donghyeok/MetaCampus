e86673c401e39a126d794537aa9a6a60772a6581
Metnias <40355580+metnias@users.noreply.github.com>
2023-06-13 09:46:12 +0900

    Edited SpawnPoint script

--- Assets/Scripts/SceneChange/SpawnPoint.cs
+++ Assets/Scripts/SceneChange/SpawnPoint.cs
@@ -2,24 +2,29 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    [SerializeField] private int myID = 0;
+    [SerializeField]
+    private int myID = -1;
 
-    [SerializeField] private Transform spawnPoint = null;
+    [SerializeField]
+    private Transform spawnPoint = null;
 
     public Vector3 GetSpawnPos() => spawnPoint.position;
 
     public int GetID() => myID;
     
-    protected virtual void PassThrough()
+    private void PassThrough()
     {
         GameManager.Instance().Spawn.SaveSpawnPoint(GetID());
     }
 
     private void OnTriggerEnter(Collider _other)
     {
-        Debug.Log("충돌");
         if (!_other.transform.root.CompareTag("Player")) return;
-        Debug.Log("플레이어 맞음");
         PassThrough();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(spawnPoint.position, 1f);
     }
 }
