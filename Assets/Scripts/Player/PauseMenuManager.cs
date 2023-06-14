8d24c82ff4acd6f9eb303c56f34f44183fd947eb
Metnias <40355580+metnias@users.noreply.github.com>
2023-06-14 17:48:34 +0900

    Added mockup for pause menu

---
+++ Assets/Scripts/Player/PauseMenuManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text txtStage;
+
+    private void Start()
+    {
+        
+    }
+
+}
