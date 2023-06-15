3fca962cff1041cacf08626e4e9151135e8871bb
Metnias <40355580+metnias@users.noreply.github.com>
2023-06-15 16:01:42 +0900

    Fixed ServerComputer

--- Assets/Scripts/StageManager/ServerComputer.cs
+++ Assets/Scripts/StageManager/ServerComputer.cs
@@ -2,16 +2,47 @@ using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class ServerComputer : MonoBehaviour
 {
+    [SerializeField]
+    private Image monitorCanvas;
+
+    [SerializeField]
+    private TMP_Text monitorText;
+
+    private void Start()
+    {
+        if (GameManager.Instance().IsDaytime())
+        {
+            monitorCanvas.color = Color.black;
+            monitorText.text = "See you later! :)";
+        }
+    }
+
+    public void OnUsed()
+    {
+        if (GameManager.Instance().IsDaytime()) return;
+        if (StageManager.Instance().IsClear) return;
+        StageManager.Instance().IsClear = true;
+        monitorCanvas.color = new Color(0.2f, 0.2f, 1f);
+        monitorText.text = "Input Received!";
+    }
+
+
+    /*
     [SerializeField] GameObject canvas = null;
     [SerializeField] GameObject detailUiPrefab = null;
     [SerializeField] Transform parentUi = null;
 
+
     private List<DataScore> dataScores = new();
+
+    
 
     private void OnTriggerEnter(Collider _other)
     {
@@ -21,9 +52,21 @@ public class ServerComputer : MonoBehaviour
             Debug.Log("플레이어맞음");
             StageManager.Instance().IsClear = true;
             Debug.Log("성적수정 성공여부: " + StageManager.Instance().IsClear);
-            StageManager.Instance().IsPlayerInServerRoom = true;
+            //StageManager.Instance().IsPlayerInServerRoom = true;
 
-            StartCoroutine(OpenLeaderboardCoroutine());
+            // StartCoroutine(OpenLeaderboardCoroutine());
+        }
+    }
+
+
+    private void OnTriggerExit(Collider _other)
+    {
+        //Debug.Log("충돌해제");
+        if (_other.transform.root.CompareTag("Player"))
+        {
+            //StageManager.Instance().IsPlayerInServerRoom = false;
+
+            //canvas.SetActive(false);
         }
     }
 
@@ -51,18 +94,6 @@ public class ServerComputer : MonoBehaviour
             if (j > 9) { yield return null; j = 0; }
         }
     }
-
-    private void OnTriggerExit(Collider _other)
-    {
-        Debug.Log("충돌해제");
-        if (_other.transform.root.CompareTag("Player"))
-        {
-            StageManager.Instance().IsPlayerInServerRoom = false;
-
-            canvas.SetActive(false);
-        }
-    }
-
     private IEnumerator DownloadScoreCoroutine()
     {
         using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/getscore.php", ""))
@@ -84,4 +115,6 @@ public class ServerComputer : MonoBehaviour
         public string id;
         public int score;
     }
+
+    */
 }
